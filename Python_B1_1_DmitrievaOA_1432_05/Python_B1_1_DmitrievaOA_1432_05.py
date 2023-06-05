c945c92ecea504c10307681a5b9efe24b33c6ad7
User <User@WIN-NCFEC32DCAH>
2023-06-05 14:47:45 +0300

    Добавьте файлы проекта.

---
+++ Python_B1_1_DmitrievaOA_1432_05/Python_B1_1_DmitrievaOA_1432_05.py
@@ -0,0 +1,13 @@
+﻿# Python_B1_1_DmitrievaOA_1432_05
+
+print("Enter a number:") # Введите число: - выводим текст на экран
+num1 = float(input()) # запрашиваем у пользователя ввод числа (и сохраняем его, приводя к типу float)
+print("Enter a boundary number:") # Введите номер границы:
+boundary = float(input()) # запрашиваем у пользователя ввод пограничного числа (и сохраняем его, приводя к типу float)
+
+if num1 < boundary: # если 1-ое число меньше пограничного
+    print("The first number is less than the boundary.") # Первое число меньше границы.
+elif num1 > boundary: # если 1-ое число больше пограничного
+    print("The first number is greater than the boundary.") # Первое число больше границы.
+    if num1 > boundary * 3: # если 1-ое число больше пограничного более, чем в 3 раза
+        print("The first number is more than 3 times greater than the boundary.") # Первое число больше границы более чем в 3 раза.
